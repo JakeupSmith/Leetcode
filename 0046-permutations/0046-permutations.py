c56090df8eb61ecc2c2bbe143c5a60ffb4542ce8
Jacob Smith <72522196+JakeupSmith@users.noreply.github.com>
2023-11-01 10:48:09 -0400

    Time: 20 ms (62.20%), Space: 13.5 MB (16.95%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def permute(self, nums):
+        def backtrack(start):
+            if start == len(nums):
+                res.append(nums[:])
+                return
+        
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start]
+        
+        res = []
+        backtrack(0)
+        return res
+        
