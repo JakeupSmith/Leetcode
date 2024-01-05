a43fca375aa948cc032f7c4b44cb063bf67443b3
Jacob Smith <72522196+JakeupSmith@users.noreply.github.com>
2024-01-04 19:01:21 -0500

    Time: 58 ms (39.01%), Space: 18.2 MB (5.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        #Hashmap Solution
-        
-        if len(s) != len(t):  #if they aren't equal lengths return false.
+        if len(s) != len(t):
             return False
         
-        countS, countT = {}, {} #creating two lists. 
+        s = sorted(s)
+        t = sorted(t)
         
-        
-        for i in range(len(s)): #We know they are equal lengths so go through the string. 
-            countS[s[i]] = 1 + countS.get(s[i], 0) #Going through s string, every charcter gets added to 1. 
-            countT[t[i]] = 1 + countT.get(t[i], 0) #Going through t string, every character gets added to 1. Default = 0 
-        for c in countS: #Go through the hashmaps and see if the counts are equal and if they aren't true, then false
-            if countS[c] != countT.get(c, 0):
-                return False 
-            
-        return True  #if not false then it must be true. 
-        
-        
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                return False
+        return True
