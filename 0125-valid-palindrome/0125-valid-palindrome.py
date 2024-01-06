81224c08af443b0f7ea4e8a142487875921cbbae
Jacob Smith <72522196+JakeupSmith@users.noreply.github.com>
2024-01-05 19:13:04 -0500

    Time: 32 ms (99.48%), Space: 17.9 MB (35.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = "" #Creating a new string to traverse the previous string. 
+        test = ""
         
-        for c in s: #Looping through every element of s.
-            if c.isalnum(): #Making sure the element is alphanumerical
-                newStr += c.lower() #making the element lower case 
-        return newStr == newStr[::-1] 
+        for i in s:
+            if i.isalnum():
+                test = test + i
     
-    #Checking if the reverse is equal to our starting string (if true then it     # is palindrome) 
+        test = test.lower()
+        
+        t = test[::-1]
+        
+        for i in range(len(test)):
+            if test[i] != t[i]:
+                return False
+        return True
     
     
             
